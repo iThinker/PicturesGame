9c8397d5f23920ea65e6d293c2fabd9ce96cf153
Roman Temchenko <temchenko.r@gmail.com>
2016-12-11 19:46:14 -0500

    Initial commit

---
+++ ios/FourPicturesGuessWord/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FourPicturesGuessWord
+//
+//  Created by Roman Temchenko on 2016-12-11.
+//  Copyright © 2016 Temkos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
